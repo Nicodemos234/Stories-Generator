3bf2699988527eb4432460a6205b0049773c0544
Nicodemos234 <nicodemos54324@gmail.com>
2021-05-22 11:27:50 -0300

    input image added

--- funcs.js
+++ funcs.js
@@ -3,10 +3,22 @@ const domtoimage = require('dom-to-image')
 
 function saveImage() {
     domtoimage.toPng(document.getElementById('image'), { quality: 1 })
-        .then(function(dataUrl) {
+        .then(function (dataUrl) {
             var link = document.createElement('a');
             link.download = 'stories.png';
             link.href = dataUrl;
             link.click();
         });
+}
+
+function loadImage() {
+    let inputObject = document.querySelector('#loaderimage');
+    let files = inputObject.files;
+    if (FileReader && files && files.length) {
+        let fr = new FileReader();
+        fr.onload = function () {
+            document.querySelector('.main-image-area img').src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
 }
