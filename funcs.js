bcc23c05ac907321549a7b2169657b570b636f9c
Nicodemos234 <nicodemos54324@gmail.com>
2021-05-28 20:53:39 -0300

    load image improvement

--- funcs.js
+++ funcs.js
@@ -2,9 +2,9 @@ const domtoimage = require('dom-to-image')
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('carregou');
-    document.querySelector('#loaderimage').addEventListener('change', () => {
-        console.log('teste');
-        let inputObject = document.querySelector('#loaderimage')
+    document.querySelector('#loaderimage').addEventListener('change', (event) => {
+        console.log(event);
+        let inputObject = event.srcElement
         let files = inputObject.files
         if (FileReader && files && files.length) {
             let fr = new FileReader()
