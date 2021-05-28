52c0479aeca25dfb076dbc186210a63ad098cd35
Nicodemos234 <nicodemos54324@gmail.com>
2021-05-28 20:46:45 -0300

    functions improvement

--- funcs.js
+++ funcs.js
@@ -1,24 +1,26 @@
-window.nodeRequire = require
 const domtoimage = require('dom-to-image')
 
-function saveImage() {
-    domtoimage.toPng(document.getElementById('image'), { quality: 1 })
-        .then(function (dataUrl) {
-            var link = document.createElement('a');
-            link.download = 'stories.png';
-            link.href = dataUrl;
-            link.click();
-        });
-}
-
-function loadImage() {
-    let inputObject = document.querySelector('#loaderimage');
-    let files = inputObject.files;
-    if (FileReader && files && files.length) {
-        let fr = new FileReader();
-        fr.onload = function () {
-            document.querySelector('.main-image-area img').src = fr.result;
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('carregou');
+    document.querySelector('#loaderimage').addEventListener('change', () => {
+        console.log('teste');
+        let inputObject = document.querySelector('#loaderimage')
+        let files = inputObject.files
+        if (FileReader && files && files.length) {
+            let fr = new FileReader()
+            fr.onload = function () {
+                document.querySelector('.main-image-area img').src = fr.result
+            }
+            fr.readAsDataURL(files[0])
         }
-        fr.readAsDataURL(files[0]);
-    }
-}
+    })
+    document.querySelector('#saveimage').addEventListener('click', () => {
+        domtoimage.toPng(document.getElementById('image'), { quality: 1 })
+            .then(function (dataUrl) {
+                var link = document.createElement('a')
+                link.download = 'stories.png'
+                link.href = dataUrl
+                link.click()
+            })
+    })
+})
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'funcs.js')
         }
     })
     win.maximize()
