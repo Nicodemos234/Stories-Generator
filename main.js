bedbef62c060ac1ec722b8ea6f10deb9809e7976
Nicodemos234 <nicodemos54324@gmail.com>
2021-05-28 21:00:17 -0300

    little refactory

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
             preload: path.join(__dirname, 'funcs.js')
         }
     })
