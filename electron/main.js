22624ab2ac25186aa3ca28ff2655094f9294e338
nicodemos234 <nicodemos54324@gmail.com>
2021-07-18 10:05:19 -0300

    Configured react in project

---
+++ electron/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+
+function createWindow() {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'funcs.js')
+        // }
+    })
+
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+
+    win.loadURL(startURL)
+    win.once('ready-to-show', () => {
+        win.show()
+        win.maximize()
+    });
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+app.on('ready', createWindow);
